dbfad35966506e2d2c7332ef38d8ea1e0c76a7a3
qpalzmm22 <qpalzmm22@gmail.com>
2021-03-08 17:50:55 +0900

    Readme.md, zlogin complete

--- tcpip/client.c
+++ tcpip/client.c
@@ -6,7 +6,7 @@
  * comment 처리된 부분만 적절하게 채워넣는다면 서버와 연결이 가능하다.
  * 서버와 연결한 후 network 관련 용어들에 대한 답을 정확하게 제시하면 다음 문제를 위한 암호를 얻을 수 있다.
  *
- * ps. 답은 영소문자로 입력해야한다.
+ * ps. 서버에서 묻는 답은 영소문자로 입력해야한다.
  * */
 
 
@@ -47,14 +47,14 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    client_sock = socket(PF_INET, SOCK_STREAM, 0);
+    client_sock = socket(/*                    */);
     if(client_sock == -1)
         error_handling("socket() error");
 
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
-    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_adr.sin_port = htons(atoi(argv[2]));
+    serv_adr.sin_addr.s_addr = /*              */;
+    serv_adr.sin_port = /*               */;
 
     serv_adr_size = sizeof(serv_adr);
 
@@ -65,14 +65,14 @@ int main(int argc, char *argv[]){
 
 
     // Receive Instruction from the server
-    str_len = read(client_sock, message_from_client, BUF_SIZE);
+    str_len = read(/*                       */);
     printf("%s\n", message_from_client);
     
 
     while(1){
 
         memset(message_from_serv, 0, BUF_SIZE);
-        str_len = read(client_sock, message_from_serv, BUF_SIZE);
+        str_len = read(/*                     */);
 
 
         if(!strncmp(message_from_serv, END_MESSAGE, str_len)) {
@@ -83,7 +83,7 @@ int main(int argc, char *argv[]){
         printf("%s\n", message_from_serv);
         
         scanf("%s", message_from_client);
-        write(client_sock, message_from_client, strlen(message_from_client));
+        write(/*                                     */);
     }
 
 
